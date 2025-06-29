3fe50cc6764fa453935127fad7260146e0c1a534
Janriz Libres <libres.janriz@gmail.com>
2025-06-29 12:07:34 +0800

    setup prettier and eslint since project is growing bigger

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,73 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...compat.extends("standard", "prettier"),
+    ],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
+      "no-undef": "off",
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling"],
+            "index",
+            "object",
+          ],
+          "newlines-between": "always",
+          pathGroups: [
+            {
+              pattern: "@app/**",
+              group: "external",
+              position: "after",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
     },
-  },
-)
+  }
+);
--- src/components/shared/WorkPanel.tsx
+++ src/components/shared/WorkPanel.tsx
@@ -1,4 +1,5 @@
 import { motion } from "motion/react";
+
 import { animateOnScroll, fadeUp } from "../../animation";
 
 interface Props {
@@ -21,29 +22,23 @@ const WorkPanel = ({
   return (
     <motion.div
       {...animateOnScroll(fadeUp)}
-      className={`cursor-default group border-gray-200 relative ${
+      className={`group relative cursor-default border-gray-200 ${
         isLast ? "border-y" : "border-t"
       }`}
     >
-      <span className="absolute -z-1 inset-0 bg-light-100 scale-x-0 origin-left transition-transform duration-400 ease-out group-hover:scale-x-100"></span>
-      <div className="custom-container px-4 sm:px-8 lg:px-12 h-32 md:h-[84px] flex items-center gap-4">
-        <div className="flex-1 text-gray-100 group-hover:text-gray-200 transition-colors duration-400 ease-out">
-          <p className="md:text-lg lg:text-2xl max-md:mb-2">{years}</p>
-          <p className="md:text-sm">{duration}</p>
+      <span className="absolute inset-0 -z-1 origin-left scale-x-0 bg-light-100 transition-transform duration-400 ease-out group-hover:scale-x-100 group-active:scale-x-100"></span>
+      <div className="custom-container flex h-36 items-center gap-4 px-4 sm:h-32 sm:px-8 md:h-[84px] lg:px-12">
+        <div className="flex-1 text-gray-100 transition-colors duration-400 ease-out group-hover:text-gray-200 group-active:text-gray-200">
+          <p className="text-2xl">{years}</p>
+          <p className="text-sm">{duration}</p>
         </div>
-        <p className="max-md:hidden flex-2 text-lg xl:text-xl group-hover:text-gray-200 transition-colors duration-400 ease-out">
+
+        <p className="flex-2 text-lg transition-colors duration-400 ease-out group-hover:text-gray-200 group-active:text-gray-200 max-md:hidden xl:text-xl">
           {company}
         </p>
-        <div className="max-md:hidden font-fira-code text-light-100 text-lg xl:text-xl flex-3 group-hover:text-gray-200 transition-colors duration-400 ease-out">
+
+        <div className="flex-3 font-fira-code text-lg text-light-100 transition-colors duration-400 ease-out group-hover:text-gray-200 group-active:text-gray-200 max-md:hidden xl:text-xl">
           <span>{role}</span> | <span>{tech}</span>
-        </div>
-        <div className="md:hidden flex-2 flex flex-col gap-2">
-          <p className="group-hover:text-gray-200 transition-colors duration-400 ease-out">
-            {company}
-          </p>
-          <div className="font-fira-code text-light-100 group-hover:text-gray-200 transition-colors duration-400 ease-out">
-            <span>{role}</span> | <span>{tech}</span>
-          </div>
         </div>
       </div>
     </motion.div>
